3360e24882de800dcf18fc2e5c26612e448bf8aa
Olha Stechenko <38553068+stechenko@users.noreply.github.com>
2024-01-29 11:55:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,53 @@ terraform {
 }
 
 
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "2.3.0"
+    }
+  }
+}
+
+variable my_count {
+  default = 600
+}
+
+resource "random_string" "random_string" {
+
+    count = var.my_count
+
+    length  = 4
+    special = false
+    upper   = true
+}
+
+
+data "vault_generic_secret" "default_terraform_version" {
+   path = "kv/tfe/repl"
+}
+
+
+
+variable "vault_address" {
+  default = "https://vault.infosec.ic1.statefarm:8200"
+}
+
+variable "vault_namespace" {
+  default = "PCAT"
+}
+
+
+provider "vault" {
+    address   = var.vault_address
+    namespace = var.vault_namespace
+
+    skip_child_token = true
+    token = "dummy"
+}
+
+
 # ----------------------------------------------------------------------------------------- #
 
 module "long-module-name-some-pet-name-should-be-here-but-i-forgot-about-it-previously" {
