579639d410914afb398294948a91755b978f72a0
Olha Stechenko <38553068+stechenko@users.noreply.github.com>
2024-05-14 10:41:55 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,35 +11,6 @@ variable my_count {
   default = 600
 }
 
-resource "random_string" "random_string" {
-
-    count = 10
-
-    length  = 4
-    special = false
-    upper   = true
-}
-
-
-data "vault_generic_secret" "default_terraform_version" {
-   path = "kv/tfe/repl"
-}
-
-
-
-variable "vault_address" {
-  default = "https://vault.infosec.ic1.statefarm:8200"
-}
-
 variable "vault_namespace" {
   default = "PCAT"
-}
-
-
-provider "vault" {
-    address   = 1212
-    namespace = 1212
-
-    skip_child_token = true
-    token = "dummy"
 }
